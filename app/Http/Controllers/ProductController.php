f958ce38daeb7edc226b5344b5d23d9cb2a7b03c
Andrey <dogin@gmail.com>
2023-06-07 00:17:17 +0300

    fix

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -72,7 +72,7 @@ class ProductController extends Controller
     public function getFile(Request $request)
     {
         $filePath = storage_path('hostingproject'.$request->input("file"));
-        return $request;
+        return $request->all();
         // return response()->file($filePath, ['Content-Type: multipart/form-data']);
     }
 
