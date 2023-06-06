7ad7a2ea8f536bcef5ea08819d2ec17fce242b48
Andrey <dogin@gmail.com>
2023-06-06 23:14:53 +0300

    fix

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -61,7 +61,7 @@ class ProductController extends Controller
             $fileName = Str::uuid();
             $file->move(storage_path(),"$fileName.$extension");
             DB::table("images")->insert([
-                "imgUrl"=>"http://localhost:8000/api/file?file=$fileName.$extension",
+                "imgUrl"=>"http://80.78.247.66//api/file?file=$fileName.$extension",
                 "products_id"=>$create
             ]);
         }
@@ -72,8 +72,8 @@ class ProductController extends Controller
     public function getFile(Request $request)
     {
         $filePath = storage_path('hostingproject'.$request->get("file"));
-        // return response()->file($filePath, ['Content-Type: multipart/form-data']);
-        return $filePath;
+        return response()->file($filePath, ['Content-Type: multipart/form-data']);
+        // return $filePath;
     }
 
     public function getOne(Request $request){
