7d5e62db323261cfb34f0989cc82337898c8e36f
Andrey <dogin@gmail.com>
2023-06-07 03:20:51 +0300

    fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,7 +43,6 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-        'cors' => \App\Http\Middleware\Cors::class, 
     ];
 
     /**
@@ -54,7 +53,6 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $middlewareAliases = [
-        'cors' => \App\Http\Middleware\Cors::class, 
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
